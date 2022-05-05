2930abf8e5a39be7b6050a56c8a30e8cb269ebd4
olaniyiOlawale <jao.olaniyi@gmail.com>
2022-05-05 08:15:56 +0100

    conditional

---
+++ main.js
@@ -0,0 +1,37 @@
+// const data = [ 'Bunmi', 'Wale' ,'Babatunde' ,'Efe' ,'Bossteeh' ,'Simeon']
+// for (let a = 0; a < data.length; a++) {
+//     const element = data[a];
+//     // console.log(element )
+
+//     if (data[a] =="Bunmi") {
+//         console.log(data[a] + " " + 'SHE IS A GIRL')
+   
+//     }        
+// else{
+//     console.log( data[a] + " " +"THEY ARE BOYS")       
+// }
+// }
+
+// let age = prompt("Enter your age")
+// if (age >= 18) {
+//     console.log("you are old enough to drive")
+// }
+// else {
+//     console.log ("you cant drive")
+// }
+// let age = 18
+// switch (age) {
+//     case "17":
+//         console.log('you cant drive')
+//         break
+//         case "20":
+//             console.log("you can drive")
+//             break
+//             default:
+//                 console.log("you are fit")
+// }
+// let isRaining = true
+// isRaining 
+// ? console.log("you need a rain coat")
+// : console.log( "no need for rain coat")
+
